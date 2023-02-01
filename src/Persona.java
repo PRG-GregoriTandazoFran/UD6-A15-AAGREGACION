90371e8cd660e90d48c6a65814855d71662b55b5
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-02-01 18:58:19 +0100

    Add Constructor

--- src/Persona.java
+++ src/Persona.java
@@ -2,11 +2,22 @@ public class Persona {
     //Agregación == No Dependencia
 
     //Atributos
-    private Nif nif;
-    private String nombre;
-    private String primerApellido;
-    private String segundoApellido;
-    private String numeroTlf;
-    private Fecha fechaNacimiento;
+    private Nif     nif;
+    private String  nombre;
+    private String  primerApellido;
+    private String  segundoApellido;
+    private String  numeroTlf;
+    private Fecha   fechaNacimiento;
     private Address address;
+
+    //Constructor
+    public Persona(Nif nif, String nombre, String primerApellido, String segundoApellido, String numeroTlf, Fecha fechaNacimiento, Address address) {
+        this.nif             = nif;
+        this.nombre          = nombre;
+        this.primerApellido  = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.numeroTlf       = numeroTlf;
+        this.fechaNacimiento = fechaNacimiento;
+        this.address         = address;
+    }
 }
