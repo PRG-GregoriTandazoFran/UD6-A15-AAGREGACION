07ffa88187c4eaed2527cdac1dcecf5962d309c1
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-02-01 18:48:06 +0100

    Copy Nif to Nif.class

--- src/Nif.java
+++ src/Nif.java
@@ -1,5 +1,38 @@
 public class Nif {
     //Atributos
-    private int dni;
+    private int  dni;
     private char letraDni;
+
+    public Nif(int dni) {
+        this.dni      = dni;
+        this.letraDni = obtenerLetraDni();
+    }
+
+    //Getters / Consultores
+    public String getNif() {
+        return String.valueOf( this.dni + this.letraDni );
+    }
+
+    public int getDni() {
+        return this.dni;
+    }
+
+    public char getLetraDni() {
+        return this.letraDni;
+    }
+
+    //Métodos
+    private int obtenerPosiccionLetra() {
+        return this.dni % 23;
+    }
+
+    private char obtenerLetraDni() {
+        char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        return letras[obtenerPosiccionLetra()];
+    }
+
+    public void mostraInfo() {
+        System.out.println( "----- TEST Clase NIF -----" );
+        System.out.printf( "El NIF del DNI %d es %s\n", this.dni, this.letraDni );
+    }
 }
