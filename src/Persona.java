5c0c9cb2f9771ccf2efc66e50528f4c5c3c96617
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-02-01 18:56:02 +0100

    Add Atributos Persona.class

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,12 @@
 public class Persona {
     //Agregación == No Dependencia
+
+    //Atributos
+    private Nif nif;
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String numeroTlf;
+    private Fecha fechaNacimiento;
+    private Address address;
 }
